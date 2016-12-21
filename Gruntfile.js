403f3274e5332b8fd176ba8494919497a2090d9b
StreamUX <streammx@naver.com>
2016-12-21 16:05:33 +0900

    node-js / 그런트 패키지 이름 수정

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON('package-grunt.json'),
 		clean: {
 			minify: [
 				'common/js/jsux*.js'
