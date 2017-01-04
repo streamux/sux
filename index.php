c3deb1f007e40bdec9e97cd3f1dad2fb59ab567d
StreamUX <streammx@naver.com>
2017-01-04 23:35:08 +0900

    index.php 오타 수정

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
 <?php
 include "config/config.inc.php";
-include "classes/context/context.class.php";
 
 $context = Context::getInstance();
 $context->init();
 
 $router = ModuleRouter::getInstance();
 $router->init();
-<<<<<<< HEAD
-=======
-
-echo _SUX_PATH_;
-
->>>>>>> develop-20161221
