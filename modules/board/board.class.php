7fec5f3147760978c705af568d278cecabeeae8d
StreamUX <streammx@naver.com>
2017-02-03 16:09:00 +0900

    board.class.php 설치 시 공지사항 자동 추가 기능 삭제

--- modules/board/board.class.php
+++ modules/board/board.class.php
@@ -5,6 +5,5 @@ class Board
 	 * @var  categories, action
 	 * They're value is used as a route uri of get method and a name of class's method
 	 */
-	static $categories = array('notice');
 	static $action = array('list','read','write','modify','reply','delete','comment','delete-comment','progress-step');
 }
