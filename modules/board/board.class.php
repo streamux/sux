26089ff5ba29fa90fd3a768e6c349ad4897cbd6b
StreamUX <streammx@naver.com>
2017-02-03 15:40:44 +0900

    board.class.php 설치 시 초기 공지사항 자동등록 카테고리명 수정

--- modules/board/board.class.php
+++ modules/board/board.class.php
@@ -5,6 +5,6 @@ class Board
 	 * @var  categories, action
 	 * They're value is used as a route uri of get method and a name of class's method
 	 */
-	static $categories = array('home');
+	static $categories = array('notice');
 	static $action = array('list','read','write','modify','reply','delete','comment','delete-comment','progress-step');
 }
