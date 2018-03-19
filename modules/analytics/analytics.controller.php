80e1c784fcbf7a5fa06ed5fcad26ed2b633da8b6
StreamUX <streammx@naver.com>
2018-03-19 11:45:05 +0900

    ..

--- modules/analytics/analytics.controller.php
+++ modules/analytics/analytics.controller.php
@@ -9,8 +9,7 @@ class AnalyticsController extends Controller
     $connectCheck = $context->getSession('connectcheck');
 
     if (empty($connectCheck)) {
-
-      $ip = $context->getServer('REMOTE_ADDR');
+      /*$ip = $context->getServer('REMOTE_ADDR');
       $now = date('Y-m-d');
       $delDate = date("Y-m-d", time() - 86400);
 
@@ -19,8 +18,8 @@ class AnalyticsController extends Controller
       $where = new QueryWhere();
       $where->set('date', $now, '=');
       $this->model->select('connect_day', '*', $where);
-
       $numrows = $this->model->getNumRows();
+
       if ($numrows > 0) {
         $row = $this->model->getRow();      
         $hit = $row['total_count']+1;
@@ -30,7 +29,7 @@ class AnalyticsController extends Controller
         $where->set('date', $now, '=');
         $this->model->update('connect_day', $column, $where);
       } else {
-        $columns = array('', 1, 0, $now);
+        $columns = array('total_count'=>1, 'real_total_count'=>1, 'date'=>$now);
         $this->model->insert('connect_day', $columns);
       }
 
@@ -47,11 +46,9 @@ class AnalyticsController extends Controller
       $where->set('ip',$ip,'=','and');
       $where->set('date',$now,'=','and');
       $this->model->select('connecter_real', '*', $where);
-
-
       $numrows = $this->model->getNumRows();
-      if (!$numrows) {
 
+      if (!$numrows) {
         $where->reset();
         $where->set('date',$delDate,'<');
         $this->model->delete('connecter_real', $where);
@@ -71,7 +68,7 @@ class AnalyticsController extends Controller
         $this->model->update('connect_day', $columns, $where);
       }
 
-      $context->setSession('connectcheck', 'yes');
+      $context->setSession('connectcheck', 'yes');*/
     }   
   }
 
@@ -79,24 +76,24 @@ class AnalyticsController extends Controller
 
     $context = Context::getInstance();
     $keyword = $context->getRequest('keyword');
-    if (isset($keyword) && $keyword) {
 
+    if (isset($keyword) && $keyword) {
       $where = new QueryWhere();
       $where->set('name', $keyword);
       $result = $this->model->select('connect_site', '*', $where);
+
       if ($result) {
-
         $rownum = $this->model->getNumRows();
+
         if ($rownum > 0) {
-
           $row = $this->model->getRow();
-          $row['hit_count'] += 1;           
-
+          $row['hit_count'] += 1;
           $columns = array('hit_count'=>$row['hit_count']);
           $this->model->update('connect_site', $columns, $where);         
         } else {
           $cachePath = './files/caches/queries/connect_site.getColumns.cache.php';
           $columnCaches = CacheFile::readFile($cachePath, 'columns');
+
           if (!$columnCaches) {
             $msg .= "QueryCacheFile Do Not Exists<br>";
             UIError::alertToBack($msg, true, array('url'=>$returnURL, 'delay'=>3));
@@ -105,22 +102,18 @@ class AnalyticsController extends Controller
 
           $queryDataes = array('name'=>$keyword, 'hit_count'=>0);
           $columns = array();
+
           for($i=0; $i<count($columnCaches); $i++) {
             $key = $columnCaches[$i];
             $value = $queryDataes[$key];
 
             if (isset($value) && $value) {
-              $columns[] = $value;
-            } else {
-              if ($key === 'date') {
-                $columns[] = 'now()';
-              } else if ($key === 'ip') {
-                $columns[] = $_SEVER['REMOTE_ADDR'];
-              }  else {
-                $columns[] = '';
-              }
-            }           
+              $columns[$key] = $value;
+            }
           }
+
+          $columns['date'] = 'now()';
+          $columns['ip'] = $_SEVER['REMOTE_ADDR'];
 
           $this->model->insert('connect_site', $columns);
         }
@@ -134,23 +127,22 @@ class AnalyticsController extends Controller
     $keyword = $context->getRequest('keyword');
 
     if (isset($keyword) && $keyword) {
-
       $where = new QueryWhere();
       $where->set('name', $keyword, '=');
       $result = $this->model->select('pageview', '*', $where);
+
       if ($result) {
-
         $rownum = $this->model->getNumRows();
-        if ($rownum > 0) {
 
+        if ($rownum > 0) {
           $row = $this->model->getRow();
           $row['hit_count'] += 1;
-
           $columns = array('hit_count'=>$row['hit_count']);
           $this->model->update('pageview', $columns, $where);         
         } else {
           $cachePath = './files/caches/queries/pageview.getColumns.cache.php';
           $columnCaches = CacheFile::readFile($cachePath, 'columns');
+
           if (!$columnCaches) {
             $msg .= "QueryCacheFile Do Not Exists<br>";
             UIError::alertToBack($msg, true, array('url'=>$returnURL, 'delay'=>3));
@@ -159,22 +151,18 @@ class AnalyticsController extends Controller
 
           $queryDataes = array('name'=>$keyword, 'hit_count'=>0);
           $columns = array();
+
           for($i=0; $i<count($columnCaches); $i++) {
             $key = $columnCaches[$i];
             $value = $queryDataes[$key];
 
             if (isset($value) && $value) {
-              $columns[] = $value;
-            } else {
-              if ($key === 'date') {
-                $columns[] = 'now()';
-              } else if ($key === 'ip') {
-                $columns[] = $_SEVER['REMOTE_ADDR'];
-              }  else {
-                $columns[] = '';
-              }       
-            }           
+              $columns[$key] = $value;
+            }         
           }
+
+          $columns['date'] = 'now()';
+          $columns['ip'] = $_SEVER['REMOTE_ADDR'];
 
           $this->model->insert('pageview', $columns);
         }
