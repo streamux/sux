737bf31ed072286094e26b4297a66561cf223d76
StreamUX <streammx@naver.com>
2018-04-07 17:23:26 +0900

    ..

--- modules/admin/tpl/admin_admin.js
+++ modules/admin/tpl/admin_admin.js
@@ -224,7 +224,7 @@ jsux.fn.main = {
       jsux.getJSON( 'http://streamux.com/board-admin/news-list-json', {}, function( e )  {
         
         if (e.result.toUpperCase() === 'Y') {
-          self.setEventNews(e.data);
+          self.setEventNews(e.data);dd
         } else {
           console.log(e.msg);
         }
